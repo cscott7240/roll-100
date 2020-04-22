5329c279da52f72a62b4e594462bda801239b09b
cscott7240 <cscott9864@gmail.com>
2020-04-22 18:43:44 -0500

    added if else statment to the final-score input

--- script.js
+++ script.js
@@ -45,10 +45,23 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     scores[activePlayer] += roundScore;
     
     // Update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]; 
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]; }
     
+    
+  var input = document.querySelector('.final-score').value;
+    var winningScore;
+    
+    if(input){
+        winningScore = input;
+    }
+    else {
+        winningScore = 100;
+    }
+        
+      
+        
     //Check if the player won the game
-    if (scores[activePlayer] >=100) {
+    if (scores[activePlayer]  >= winningScore) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -58,11 +71,14 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     else {
     
     //Next player
-    nextPlayer();}
+    nextPlayer();
     }
+    });
     
     
-});
+    
+    
+
 
 function nextPlayer() {
    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
